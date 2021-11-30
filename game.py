e1cb65a2356f0cc02c486266b1de65229594c1d5
Joanna Lee <joanna.lee56@gmail.com>
2021-11-30 12:03:25 -0800

    Get random number

--- game.py
+++ game.py
@@ -1,4 +1,11 @@
 """A number-guessing game."""
 
+import random
+
 print("Greetings, welcome to the Guessing Game!")
 player_name = input("What is your name? ")
+
+print(f"{player_name}, I'm thinking of a number between 1 and 100. Try to guess the number.")
+
+secret_number = random.randint(0, 101)
+print(secret_number)
