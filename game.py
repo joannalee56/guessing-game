a23dda0e63833a9723e10d72a8b6d79ab94c00ac
Joanna Lee <joanna.lee56@gmail.com>
2021-11-30 16:24:49 -0800

    Multiple Rounds (working in progress)

--- game.py
+++ game.py
@@ -9,15 +9,17 @@ print("Greetings, welcome to the Guessing Game!")
 player_name = input("What is your name? ")
 
 # Get random number
+
 print(f"{player_name}, I'm thinking of a number between 1 and 100. Try to guess the number.")
 
 secret_number = random.randint(1, 101)
 print(secret_number)
 
-# Cycles through until the user guesses the right number
-
+    # Cycles through until the user guesses the right number
+play_game = True
 counter = 0
-while True:
+scores = []
+while play_game == True:
     try:
         # Prompt player to guess a number
         guess_number = int(input("Guess a number between 1 and 100: "))
@@ -34,6 +36,26 @@ while True:
             # Ends when the user guesses the right number and displays the number of guesses that the user made
             else:
                 print(f"Well done, {player_name}! You found my number in {counter} tries!")
-                break
+                scores.append(counter)
+                print(scores)
+
+# This is not working, the Y/N?
+                continueGame = True
+                while continueGame == True:
+                    play_again = input("Would you like to play again? Y/N: ")
+                    if play_again == "Y" or play_again == "y":
+                        play_game == True
+                        continueGame == False
+                        secret_number = random.randint(1, 101)
+                        print(secret_number)
+                        counter = 0
+                    elif play_again == "N" or play_again == "n":
+                        play_game == False
+                        continueGame == False
+                        break
+                    else:
+                        print("Please enter Y or N.")
+                        continueGame == True
+                    
     except ValueError:
         print("Oops! That was not a number. Please enter a valid number.")
